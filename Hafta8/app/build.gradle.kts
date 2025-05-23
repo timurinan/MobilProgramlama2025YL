259b1db1bce8bab7f0ec5b648a6e3f36ce4471b0
timurinan <91524596+timurinan@users.noreply.github.com>
2025-05-23 12:52:12 +0300

    23/05/2025

--- Hafta8/app/build.gradle.kts
+++ Hafta8/app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.myapplication"
+    namespace = "com.example.loginsgninapp"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.myapplication"
+        applicationId = "com.example.loginsgninapp"
         minSdk = 27
         targetSdk = 33
         versionCode = 1
@@ -37,7 +37,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
-    implementation("com.google.firebase:firebase-auth:23.2.0")
+    implementation("com.google.firebase:firebase-auth:23.2.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
